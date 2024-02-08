ca476e2290a703844b98fc6b692d50bf52fed17c
IharTsykala <ihatsykala@kitrum.com>
2024-02-08 22:39:53 +0600

    feat: 1-3 modules

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,20 @@
 // Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 const testCases = [
-    { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+  { a: 11, b: 22, action: Action.Add, expect: 33 },
+  { a: 22, b: 22, action: Action.Add, expect: 44 },
+  { a: 33, b: 22, action: Action.Add, expect: 55 },
+  { a: 44, b: 33, action: Action.Subtract, expect: 11 },
+  { a: 44, b: 10, action: Action.Multiply, expect: 440 },
+  { a: 440, b: 10, action: Action.Divide, expect: 44 },
+  { a: 9, b: 2, action: Action.Exponentiate, expect: 81 },
+  { a: 44, b: 33, action: 'Action.Exponentiate', expect: null },
+  { a: 44, b: '33', action: Action.Add, expect: null },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  test.each(testCases)('test case № %#', (testCases) => {
+    expect(simpleCalculator({ ...testCases })).toEqual(testCases.expect);
   });
-  // Consider to use Jest table tests API to test all cases above
 });
