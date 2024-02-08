a73e1b8a50cfd89c1a71e9f491fc17796a8b83e0
IharTsykala <ihatsykala@kitrum.com>
2024-02-09 02:42:41 +0600

    feat: 7 module

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,71 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('lodash/throttle', () => ({
+  default: jest.fn((fn) => fn),
+  __esModule: true,
+}));
+
+jest.mock('axios');
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+const RELATIVE_PATH = '/example';
+const TIMEOUT = 10000;
+const METHOD = 'create';
+const DATA_MOCK = {
+  name: 'Ihar',
+  surnname: 'Tsykala',
+};
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const mockReturnObject = {
+      get: jest.fn().mockReturnValue({ data: {} }),
+    };
+    const axiosResultMock = jest
+      .spyOn(axios, METHOD)
+      .mockReturnValue(mockReturnObject as any);
+
+    await throttledGetDataFromApi(BASE_URL);
+
+    expect(axiosResultMock).toHaveBeenCalledWith({
+      baseURL: BASE_URL,
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const getMockAxios = jest.fn().mockReturnValue({ data: {} });
+    const mockReturnObject = {
+      get: getMockAxios,
+    };
+
+    jest.spyOn(axios, METHOD).mockReturnValue(mockReturnObject as any);
+
+    jest.advanceTimersByTime(TIMEOUT);
+    await throttledGetDataFromApi(RELATIVE_PATH);
+
+    expect(getMockAxios).toHaveBeenCalledWith(RELATIVE_PATH);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const mockReturnObject = {
+      get: jest.fn().mockReturnValue({ data: DATA_MOCK }),
+    };
+
+    jest.spyOn(axios, METHOD).mockReturnValue(mockReturnObject as any);
+
+    jest.advanceTimersByTime(TIMEOUT);
+
+    expect(await throttledGetDataFromApi(RELATIVE_PATH)).toEqual(DATA_MOCK);
   });
 });
